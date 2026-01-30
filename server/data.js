dd0205bb0cd1ce995c2d3f041a9ab971993a3a2c
ruchanimbalkar <rucha.nimbalkar90@gmail.com>
2026-01-30 13:54:51 -0800

    first commit

---
+++ server/data.js
@@ -0,0 +1,254 @@
+export default [
+  {
+    episodeNo: 1,
+    episodeTitle: "Satyanweshi",
+    description:
+      "In 1932, Byomkesh Bakshi investigates murders that have taken place in the Chinatown locality. He assumes an alias and starts living in the hostel run by the homeopathic doctor Anukul babu. Initially told that there weren't rooms available, he convinces Ajit Kumar Banerjee to share a room with him. Then, by applying deductive methods, he soon identifies the murderer and induces an attack from him, and thereby catching him red-handed.",
+    season: 1,
+    youTubeLink: "https://youtu.be/dpdo8t2F8cQ?si=AOlf3fXPUTJXE5U_",
+  },
+  {
+    episodeNo: 2,
+    episodeTitle: "Raste Ka Kanta",
+    description:
+      "In 1933, an advertisement in the newspaper draws the attention of Bakshi. Although seemingly funny, this advertisement Agar aap raaste ke kaante se... it seems to have a more dangerous implication. A client approaches Bakshi and narrates how he was almost pierced by an object while walking on the street. Byomkesh investigates the matter and catches this dangerous person who has a special method of killing people.",
+    season: 1,
+    youTubeLink: "https://youtu.be/rCgJiDnT2Ws?si=ZtRLRJcsqJ65Crz6",
+  },
+  {
+    episodeNo: 3,
+    episodeTitle: "Seemant Heera",
+    description:
+      "The case revolves in 1934 around the theft of a priceless heirloom, and Byomkesh and Ajit are invited by the current heir to recover it from the heir's uncle, as consumed by greed, he has stolen it. His uncle offers to let Byomkesh and Ajit search the house for seven days. Byomkesh cracks the culprit's expertise in sculpting and eventually succeeds in getting the Seemant Heera back",
+    season: 1,
+    youTubeLink: "https://youtu.be/Igj63T-rJpc?si=9aIHcWuwsgeqMPT0",
+  },
+  {
+    episodeNo: 4,
+    episodeTitle: "Makdi Ka Ras",
+    description:
+      "In 1934, Byomkesh meets Mohan, one of Ajit's school friends and a doctor by profession who shares an intriguing event with them and seeks Bakshi's help. One of his patients, Nand Dulal Babu, has a weird addiction to Tarantula's fluid, which is gradually damaging his health. Despite the strict vigilance, how the fluid reaches Nandlal babu becomes the biggest question. But Bakshi solves the case in no time.",
+    season: 1,
+    youTubeLink: "https://youtu.be/NSFFFTmZsy8?si=IPkcl1QL7tuR36Hz",
+  },
+  {
+    episodeNo: 5,
+    episodeTitle: "Wasiyat",
+    description:
+      "In 1935, Karali Babu, a wealthy Bengali, with no direct heir, has been murdered in his sleep. As Byomkesh determines later in the story, the victim was killed in such a way that hints at the culprit having knowledge of medical procedures. The fact that the victim was in the habit of frequently changing his will out of temper makes it likely that anyone who could inherit his wealth would have killed him. Byomkesh probes the case and gets hold of the murderer. During this case, he meets Satyabati for the first time who is shown as his wife in the later episodes.",
+    season: 1,
+    youTubeLink: "https://youtu.be/qEswKEZ10YE?si=G8LsfAHnpzeoE1cM",
+  },
+  {
+    episodeNo: 6,
+    episodeTitle: "Ret ka Daldal (Quicksand)",
+    description:
+      "During a vacation in 1936, Byomkesh and Ajit meet a financially troubled landlord whose daughter's tutor has strangely gone missing with the account books of his estate. The unexpected solution of the case revolves around a patch of quicksand somewhere near the landlord's residence.",
+    season: 1,
+    youTubeLink: "https://youtu.be/nCWPn-8RTB4?si=rC-a-6KOOzbh7N5i",
+  },
+  {
+    episodeNo: 7,
+    episodeTitle: "Laal Neelam",
+    description:
+      "In 1936, while Byomkesh talks about the release from jail of a criminal with a record of theft, loot, fraud and many such crimes, Ramanath Niyogi, he gets news regarding Maharaja Ramendra Singh's secretary, Harihar's murder. Harihar, who used to be a criminal years ago but had mended his ways later, was pitilessly killed; Singh insists Bakshi look into the matter and find out the culprit, which he eventually succeeds in. Since this episode, Satyabati is shown as Byomkesh's wife.",
+    season: 1,
+    youTubeLink: "https://youtu.be/n5ugW2eskMM?si=64A1fiEHkfVZzIL-",
+  },
+  {
+    episodeNo: 8,
+    episodeTitle: "Bhoot",
+    description:
+      "In 1936, Byomkesh investigates a case about allegedly a ghost haunting a house. He eventually succeeds in finding the person, who posed as the ghost to search and steal diamonds from the house",
+    season: 1,
+    youTubeLink: "https://youtu.be/eniPTmLlRCo?si=3PPiifoeNRR_yzD-",
+  },
+  {
+    episodeNo: 9,
+    episodeTitle: "Agnibaan",
+    description:
+      "In 1944, as the daughter of Professor Deb Kumar dies mysteriously in the kitchen, while the former is in the Science Congress in Delhi (and also just after a new insurance policy venture of having the husband and wife, in which if one dies the other gets all the insurance), Byomkesh Bakshi decides to investigate the case himself and determine the cause of her death",
+    season: 1,
+    youTubeLink: "https://youtu.be/NokiooOu6wk?si=vskPFLmGNBTcjClZ",
+  },
+  {
+    episodeNo: 10,
+    episodeTitle: "Upasanhaar",
+    description:
+      "In 1945, Byomkesh gets a set deadly matches (of Professor Deb Kumar) when he was about to smoke. He asked everyone, including Puntiram and Satyavati. Then he remembered that it was a muffled man who asked his matches to smoke. Later, he got a letter from a Kokanand Gupta to Byomkesh Bose. Bakshi never thought that there was another Byomkesh in the locality. Eventually, the person who wanted Bakshi to die was none other than Anukul babu from Satyanweshi, who decided to take revenge against Byomkesh.",
+    season: 1,
+    youTubeLink: "https://youtu.be/6koJ9opUDO4?si=SW8Y0BiCUIBihh8p",
+  },
+  {
+    episodeNo: 11,
+    episodeTitle: "Tasvir Chor",
+    description:
+      "When a group photo gets missing and a painter, Phalguni Pal gets murdered, Inspector Purandar Pandey requests Byomkesh to find the murderer and thief",
+    season: 1,
+    youTubeLink: "https://youtu.be/H_ZisDw9t14?si=I8_lFZgRluCuju7l",
+  },
+  {
+    episodeNo: 12,
+    episodeTitle: "Kile Ka Rahasya",
+    description:
+      "Byomkesh and Ajit visit a fort, where they meet their police friend Purandar Pandey. When a professor dies seemingly by snakebite, the reactions of others lead Byomkesh to deduce that there was more than what meets the eye.",
+    season: 1,
+    youTubeLink: "https://youtu.be/bb-kGfRIpKg?si=x_q46K8oWcjR5WGE",
+  },
+  {
+    episodeNo: 13,
+    episodeTitle: "Chiriya Ghar (Part 1) ",
+    description:
+      "Byomkesh is approached by a new client, Nishanath Sen, to find out if a yesteryear actress is living incognito, among others, in his household.",
+    season: 1,
+    youTubeLink: "https://youtu.be/3bTGfpcDvrw?si=UYcEVX0yWtEpUCcz",
+  },
+  {
+    episodeNo: 13,
+    episodeTitle: "Chiriya Ghar (Part 2) ",
+    description:
+      "When Nishanath is murdered, Byomkesh decides to probe into the case with the help of his close aide, Ajit",
+    season: 1,
+    youTubeLink: "https://youtu.be/TocXQVU6FOQ?si=rsfqH5t5awBNUVPn",
+  },
+  {
+    episodeNo: 14,
+    episodeTitle: "Aadim Shatru (Part 1) ",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/XysPMcA4d9Y?si=Asea0kvUSd1Pvdif",
+  },
+  {
+    episodeNo: 14,
+    episodeTitle: "Aadim Shatru (Part 2) ",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/gKzHcph0lnI?si=CTfs4Sgl61hTNtz4",
+  },
+  {
+    episodeNo: 15,
+    episodeTitle: "Aag Aur Patanga",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/Lce6PEGDWA0?si=X08h-X4SMxlfoN2C",
+  },
+  {
+    episodeNo: 16,
+    episodeTitle: "Vansh Ka Khoon",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/GrVT_ynauHw?si=3UXSCOGrcmJt26gX",
+  },
+  {
+    episodeNo: 17,
+    episodeTitle: "Necklace",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/8-rbR3d72A4?si=z0tTFuRcoveSatfZ",
+  },
+  {
+    episodeNo: 18,
+    episodeTitle: "Amrit ki Maut",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/UR7Vcm1G9P4?si=pOsdJunrVzuLfkyp",
+  },
+  {
+    episodeNo: 19,
+    episodeTitle: "Pahari Rahasya",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/gtqkVL0Ijgk?si=yCp7k5R7y0LYiLSG",
+  },
+  {
+    episodeNo: 20,
+    episodeTitle: "Anjaan Khooni",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/MMVhPBNiK8c?si=8TRY3OEWctE9-KNG",
+  },
+  {
+    episodeNo: 21,
+    episodeTitle: "Kahen Kavi Kalidas",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/Sdi-HrQuPPU?si=nG-aaflGBa4ROJDE",
+  },
+  {
+    episodeNo: 22,
+    episodeTitle: "Adrishya Trikon",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/xvJNxZY4IrQ?si=bFSETv_jSpqqnXTF",
+  },
+  {
+    episodeNo: 23,
+    episodeTitle: "Wasiyat ka Rahasya",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/uQVRfcNbKkk?si=Gp57xB6Z9GczKk69",
+  },
+  {
+    episodeNo: 24,
+    episodeTitle: "Bemisaal",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/oKslMvJznVI?si=sm8Z0-xg8sOoDauN",
+  },
+  {
+    episodeNo: 25,
+    episodeTitle: "Balak Jasoos",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/MGEcqJhglaY?si=ilu7oa71SLD_Gu67",
+  },
+  {
+    episodeNo: 26,
+    episodeTitle: "Chakrant",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/Tkvqg8iZBwE?si=4U_3JedIJJvE0VFU",
+  },
+  {
+    episodeNo: 27,
+    episodeTitle: "Paheli Gatha",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/26-s7bB8BUo?si=r3jk4-te5HCRdimJ",
+  },
+  {
+    episodeNo: 28,
+    episodeTitle: "Kamra No. 102",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/NJGoMJxPNjI?si=TMAIuNyehGFlA5Jf",
+  },
+  {
+    episodeNo: 29,
+    episodeTitle: "Dhokadhari",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/4dm1jbhvdFQ?si=vbZbgXs59FbK6txR",
+  },
+  {
+    episodeNo: 30,
+    episodeTitle: "Sahi ka Kanta",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/fudbHt3zAzE?si=2nF71_YruZfKWSyL",
+  },
+  {
+    episodeNo: 31,
+    episodeTitle: "Veni Sanhar",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/C0KFS1_7hjU?si=O7fTfZvc6GKErqAy",
+  },
+  {
+    episodeNo: 32,
+    episodeTitle: "Lohe ka Biscuit",
+    description: "",
+    season: 2,
+    youTubeLink: "https://youtu.be/nYYOc6pkxls?si=Q6FCFZGvtT0BLU_b",
+  },
+];
