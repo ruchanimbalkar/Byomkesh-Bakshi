dce53f993fa441722f65c42f5307f7306a5f1103
ruchanimbalkar <rucha.nimbalkar90@gmail.com>
2026-02-03 09:56:23 -0800

    updated connection.js in backend

--- server/connection.js
+++ server/connection.js
@@ -7,25 +7,14 @@ const uri = process.env.ATLAS_URL;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri);
 
-// let connection;
-// try {
-//   connection = await client.connect();
-//   console.log("connected!");
-// } catch (error) {
-//   console.log(error);
-// }
-
-//const db = connection.db("byomkeshbakshi_ddserial");
-
-async function run() {
-  try {
-    const db = client.db("byomkeshbakshi_ddserial");
-    const result = await db.collection("episodes").insertMany(documents);
-    console.log(`${result.insertedCount} docs added!`);
-  } finally {
-    await client.close();
-  }
+let connection;
+try {
+  connection = await client.connect();
+  console.log("connected!");
+} catch (error) {
+  console.log(error);
 }
 
-run().catch(console.dir);
-//export default db;
+const db = connection.db("byomkeshbakshi_ddserial");
+
+export default db;
